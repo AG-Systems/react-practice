0d801a61ba73794d17d367799bd6a5ba1dc9b988
Auriga <AG-Systems@users.noreply.github.com>
2016-10-17 12:00:14 -0700

    Update app.js

--- tweetbox/app.js
+++ tweetbox/app.js
@@ -1,1 +1,33 @@
 
+var TweetBox = React.createClass({
+    getInitialState: function() {
+    return {
+      text: ""
+    };
+  },
+  handleChange: function(event) 
+  {
+     this.setState({ text: event.target.value });
+     console.log(event.target.value);
+  },
+  render: function() 
+  {
+    return (
+      <div className="well clearfix">
+        <textarea className="form-control" onChange={this.handleChange}></textarea>
+        <br/>
+        <span>{140 - this.state.text.length}</span>
+        <button className="btn btn-primary pull-right" disabled={this.state.text.length === 0}>Tweet</button>
+     {/*         <br/>
+      {this.state.text}
+      This outputs what the input is typed in. 
+     */}
+      </div>
+      )
+  }
+});
+
+ReactDOM.render(
+  <TweetBox/>, 
+  document.getElementById('container')
+);
