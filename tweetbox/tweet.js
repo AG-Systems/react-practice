b80112d1fdf337153056508575adda79b281fca6
Auriga <AG-Systems@users.noreply.github.com>
2016-10-17 13:13:56 -0700

    Update tweet.js

--- tweetbox/tweet.js
+++ tweetbox/tweet.js
@@ -1,9 +1,27 @@
-
 var TweetBox = React.createClass({
     getInitialState: function() {
     return {
       text: ""
     };
+  },
+  overflowAlert: function() {
+if (this.remainingCharacters() < 0) {
+  var beforeOverflowText = this.state.text.substring(140 - 10, 140);
+  var overflowText = this.state.text.substring(140);
+
+  return (
+    <div className="alert alert-warning">
+      <strong>Oops! Too Long:</strong>
+      &nbsp;...{beforeOverflowText}
+      <strong className="bg-danger">{overflowText}</strong>
+    </div>
+  );
+}
+},
+
+  remainingCharacters: function() 
+  {
+      return 140 - this.state.text.length;
   },
   handleChange: function(event) 
   {
@@ -14,6 +32,7 @@ var TweetBox = React.createClass({
   {
     return (
       <div className="well clearfix">
+        { this.overflowAlert() }
         <textarea className="form-control" onChange={this.handleChange}></textarea>
         <br/>
         <span>{140 - this.state.text.length}</span>
