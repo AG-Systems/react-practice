6261ee58945c33a25927275e623372f1d2b1c7d8
Auriga <AG-Systems@users.noreply.github.com>
2016-10-16 17:19:14 -0700

    Update grid.js

--- basic-image-grid/grid.js
+++ basic-image-grid/grid.js
@@ -1,1 +1,42 @@
+var List = React.createClass({
+    render: function() {
+      return (
+        <ul>
+          {this.props.list.map( image =>
+            <li>
+              <img {...image} />
+            </li>
+          )}
+        </ul>
+      )
+    }
+  });
+  
+var Playlist = React.createClass({
+  render() {
+    let playlistImages = {
+          "images": [
+            {
+              "src": "http://placehold.it/350x150",
+              "className": "playlist-tn"
+            },
+            {
+              "src": "http://placehold.it/350x150",
+              "className": "playlist-tn"
+            },
+            {
+              "src": "http://placehold.it/350x150",
+              "className": "playlist-tn"
+            }
+          ]
+        }
+    return (
+        <List list={playlistImages.images} />
+    )
+  }
+});
 
+ReactDOM.render(
+  <Playlist />,
+  document.getElementById('container')
+);
